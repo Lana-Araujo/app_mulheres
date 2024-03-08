f482b9edcf484176bf86256d16fd4d32797faaaa
Lana Araújo <lana.academico2@gmail.com>
2024-03-08 10:43:16 -0300

    index

---
+++ web/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+<div class="info">
+            <div class="logo-nav">
+                <img id="logo-nav" src="src/general/header-logo.png">
+            </div>
+            <div class="nav-wrap">
+                <ul class="menu">
+                    <li><a href="index.php">Página Inicial</a></li>
+                    <li><a href="sobrenos.php"></a></li>
+                    <li><a href="faleconosco.php">Contato</a></li>
+                    <li>
+                        <a href="mercado.php">Mercado</a>
+                        <ul class="submenu">
+                            <li><a href="#">Produtos</a></li>
+                            <li><a href="#">Soluções Tecnológicas</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div id="botao">
+                    <a href="loginnovo.php"><button id="login">Login</button></a>
+                </div>
+            </div>
+        </div>
+    
+</body>
+</html>
